d9dbae43dfdac82ca74cce0bb0355de16efe2aa2
Avi7877489 <avi7877489@gmail.com>
2024-10-06 23:41:31 +0530

    Prime number Checker

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let arr = [4,5,6,2,1,77,8,9]
 let count= 0
 for(let i = 0;i<=arr.length;i++){
-    if(arr[i] % 2 !== 0){
-        console.log(arr[i] + " - This is Odd Number")
+    for(let j= 1;j<arr[i];j++){
+    if(arr[i] % 2 == 0){
+        count++
+    }
+    }
+    if(count == 2){
+        console.log(arr[i] + " - This is prime Number")
     }
 
 }
