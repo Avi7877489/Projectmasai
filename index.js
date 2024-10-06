d4b717decf0963f9a2653983ca850adaa02d2f59
Avi7877489 <avi7877489@gmail.com>
2024-10-07 00:02:50 +0530

    Primr number Cheaker

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let arr = [4,5,6,2,1,77,8,9]
-let count= 0
-for(let i = 0;i<=arr.length;i++){
-    for(let j= 1;j<arr[i];j++){
-    if(arr[i] % 2 == 0){
+for(let i = 0;i< arr.length;i++){
+    let count= 0
+    for(let j= 1;j<=arr[i];j++){
+    if(arr[i] % j == 0){
         count++
+        }
     }
-    }
-    if(count == 2){
-        console.log(arr[i] + " - This is prime Number")
+
+    if(count === 2){
+        console.log(arr[i] , " - This is prime Number")
     }
 
 }
